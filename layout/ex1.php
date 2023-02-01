73f87d3d6fb04b64e19c03e893970e1fe12e4496
ahmadomid1984 <111872990+ahmadomid1984@users.noreply.github.com>
2023-02-01 11:29:26 +0200

    exercise 1 is done

--- layout/ex1.php
+++ layout/ex1.php
@@ -17,5 +17,39 @@
     echo date("d.m.Y");
     ?>
 
-<?php include "footer.php" ?>
+    <h1> 3.4 Variables in PHP </h1>
 
+    <?php
+    $title1 = "PHP is interesting";
+    echo "<h1>" . $title1 . "</h1>";
+    ?>
+
+    <h1> 3.5 Tables & Variables </h1>
+
+    <?php
+    $g1 = 5;
+    $g2 = 4;
+    $g3 = 5;
+    echo "
+
+    <table>
+    <tr>
+    <th> S.N </th> <th> Name </th> <th> Grade </th>
+    </tr>
+    <tr>
+    <td> 1 </td> <td> Pekka </td> <td> $g1 </td>
+    </tr>
+    <tr>
+    <td> 2 </td> <td> Johanna </td> <td> $g2 </td>
+    </tr>
+    <tr>
+    <td> 3 </td> <td> John </td> <td> $g3 </td>
+    </tr>
+    </table>
+    ";
+    ?>
+
+    <h1> 4 Screenshot of the development environment </h1>
+    <img src="Docker Desktop.jpg" alt="screenshot">
+
+<?php include "footer.php" ?>
